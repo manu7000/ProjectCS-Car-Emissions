673e16f91dd82b40d486f84966edbd333eeb81b4
manu7000 <eblanchard1@pm.me>
2025-05-02 13:25:41 +0200

    Removed test feature

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from Feature_01 import return_even
 
 # ---- PAGE SETUP ----
 st.set_page_config(page_title="Car Journey CO₂ Emission Calculator", page_icon="🚗", layout="centered")
