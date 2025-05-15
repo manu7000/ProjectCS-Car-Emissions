c62a0db26493047944312f577d37fab65f0b489a
manu7000 <eblanchard1@pm.me>
2025-05-15 16:25:54 +0200

    Added exception if no route found

--- Map_API.py
+++ Map_API.py
@@ -1,4 +1,5 @@
 import openrouteservice
+from openrouteservice import exceptions
 
 client = openrouteservice.Client(key='5b3ce3597851110001cf624863c2387f20a145d69082b4da269112fa') 
 
@@ -19,16 +20,20 @@ def autocomplete_address(partial_text):
         raise RuntimeError(f"Autocomplete failed: {e}")
 
 def get_route_info(start_coords, end_coords):
-    route = client.directions(
-        coordinates=[start_coords, end_coords],
-        profile='driving-car',
-        format='geojson'
-    )
-    segment = route['features'][0]['properties']['segments'][0]
-    geometry = route['features'][0]['geometry']['coordinates']
-    return {
-        "distance_km": segment['distance'] / 1000,
-        "duration_min": segment['duration'] / 60,
-        "geometry": geometry
-    }
+    try:
+        route = client.directions(
+            coordinates=[start_coords, end_coords],
+            profile='driving-car',
+            format='geojson'
+        )
+        segment = route['features'][0]['properties']['segments'][0]
+        geometry = route['features'][0]['geometry']['coordinates']
+        return {
+            "distance_km": segment['distance'] / 1000,
+            "duration_min": segment['duration'] / 60,
+            "geometry": geometry
+        }
+    except exceptions.ApiError as e:
+        print(f"OpenRouteService API Error: {e}")
+        return None 
 
